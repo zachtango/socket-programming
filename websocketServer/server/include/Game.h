c4bce29ef14682c9e88cf51e2dbbaec50f0b07d3
Zach Tang <zachtangoj@gmail.com>
2023-07-29 23:48:13 -0700

    game interface implementation

---
+++ websocketServer/server/include/Game.h
@@ -0,0 +1,26 @@
+#include "json.hpp"
+
+namespace ClientMessageType {
+    constexpr const char *PlayAgain = "PlayAgain";
+    constexpr const char *Move = "Move";
+
+};
+
+enum class Player {One, Two, Spectator};
+enum class Square {O, X, Empty};
+enum class GameState {Idle, Active, Finished};
+
+class Game {
+
+public:
+    virtual void AddPlayer(int playerId) = 0;
+    virtual Player GetPlayer(int playerId) = 0;
+    virtual bool Move(int x, int y) = 0;
+    virtual bool StartGame() = 0;
+    virtual Player Winner() = 0;
+    virtual bool Reset() = 0;
+    virtual nlohmann::json Json() = 0;
+    virtual void Print() = 0;
+
+};
+
